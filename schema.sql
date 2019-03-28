ba2f5cfbca232ec16abdec08bebf309fb8c11e63
nelsonic <nelson+github@dwyl.io>
2019-03-28 21:19:26 +0000

    update database name in all places (duh!)

--- schema.sql
+++ schema.sql
@@ -2,26 +2,25 @@
 -- first drop test tables from previous session so we have a clean database */
 DROP SCHEMA public cascade; /* http://stackoverflow.com/a/13823560/1148249 */
 CREATE SCHEMA IF NOT EXISTS public;
--- DROP DATABASE IF EXISTS test;
-CREATE DATABASE codeface;
+-- DROP DATABASE IF EXISTS codeface;
+-- CREATE DATABASE codeface;
 /* create the people table */
 CREATE TABLE IF NOT EXISTS people (
   id SERIAL PRIMARY KEY,
   inserted_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   name VARCHAR(100) DEFAULT NULL,
   username VARCHAR(50) DEFAULT NULL,
-  uid INT NOT NULL
+  uid INT DEFAULT NULL
 );
 
 /* insert a person into the people table if it does not already exist */
 /* stackoverflow.com/questions/4069718/postgres-insert-if-does-not-exist */
-INSERT INTO people (name, username, uid)
-  SELECT name, username, uid FROM people
+INSERT INTO people (name, username)
+  SELECT name, username FROM people
   UNION
   VALUES (
     'Jimmy Testuser',
-    'jimmy',
-    '7126237'
+    'jimmy'
   )
   EXCEPT
-  SELECT name, username, uid FROM people;
+  SELECT name, username FROM people;
--- test/_create_table.js
+++ test/_create_table.js
@@ -1,35 +1,51 @@
-var DBURL = process.env.DATABASE_URL || "postgres://postgres:@localhost/test";
-var assert = require('assert');
-var pg = require('pg');
+const DBURL = process.env.DATABASE_URL
+  || "postgres://postgres:@localhost/codeface";
+const assert = require('assert');
+const pg = require('pg');
+const resolve = require('path').resolve;
+const read = require('fs').readFileSync;
+const client = new pg.Client(DBURL); // Create PostgreSQL Client
+
+// NEXT: use https://github.com/dwyl/ordem to run the tasks ...
+
+
+const schema = read(resolve('./schema.sql'), 'utf8').toString();
+console.log('\n', schema);
 
 /**
  * create_tables sets up the database tables we need for the MVP app
  * @param  {Function} callback called if/when the SQL script succeeds!
  */
 function create_tables (callback) {
-  var client = new pg.Client(DBURL);
-  client.connect(function(err) {
-    if (err) {
-      console.log(' - - - - - - - - -err:')
-      console.log(err);
-      console.log('- - - - - - - - - - - - - - - - - - - ');
-      assert(!err); // "die" if we cannot connect to the database!
-    }
-    var file = require('path').resolve('./schema.sql');
-    var query = require('fs').readFileSync(file, 'utf8').toString();
-    // console.log('\n', query);
-    client.query(query, function(err, result) {
-      console.log(err, result, 'DB Table Created & Test Data Inserted');
-      var SELECT = 'SELECT * FROM people';
-      client.query(SELECT, function(err, result) {
 
-        client.end(); // close connection to database
-        return callback(err, result);
-      })
-    });
+  client.query(schema, function(err, result) {
+    if (err) {
+      console.error(err);
+    }
+    console.info('DB Table Created & Test Data Inserted');
+    var SELECT = 'SELECT * FROM people';
+    client.query(SELECT, function(err, result) {
+
+      client.end(); // close connection to database
+      return callback(err, result);
+    })
   });
+
 }
 
-create_tables(function (err, result) {
-  console.log(err, 'result:', result.rows[0]);
+client.connect(function(err) {
+  if (err) {
+    console.log(' - - - - - - - - - - - - - - - -> err:')
+    console.error(err);
+    console.log('- - - - - - - - - - - - - - - - - - - ');
+    assert(!err); // "die" if we cannot connect to the database!
+  }
+  else {
+    create_tables(function (err, result) {
+      if (err) {
+        console.error(err);
+      }
+      console.log('result:', result.rows[0]);
+    });
+  }
 });
