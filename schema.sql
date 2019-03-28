e99e5c6421748cac686dafd95e53baf8a6711f30
nelsonic <nelson+github@dwyl.io>
2019-03-28 21:12:59 +0000

    attempt to change name of database in schema.sql #51

--- schema.sql
+++ schema.sql
@@ -1,25 +1,27 @@
 -- echo-all
 -- first drop test tables from previous session so we have a clean database */
--- DROP SCHEMA public cascade; http://stackoverflow.com/a/13823560/1148249 */
--- CREATE SCHEMA IF NOT EXISTS public;
+DROP SCHEMA public cascade; /* http://stackoverflow.com/a/13823560/1148249 */
+CREATE SCHEMA IF NOT EXISTS public;
 -- DROP DATABASE IF EXISTS test;
--- CREATE DATABASE test;
+CREATE DATABASE codeface;
 /* create the people table */
 CREATE TABLE IF NOT EXISTS people (
   id SERIAL PRIMARY KEY,
   inserted_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   name VARCHAR(100) DEFAULT NULL,
-  username VARCHAR(50) DEFAULT NULL
+  username VARCHAR(50) DEFAULT NULL,
+  uid INT NOT NULL
 );
 
 /* insert a person into the people table if it does not already exist */
 /* stackoverflow.com/questions/4069718/postgres-insert-if-does-not-exist */
-INSERT INTO people (name, username)
-  SELECT name, username FROM people
+INSERT INTO people (name, username, uid)
+  SELECT name, username, uid FROM people
   UNION
   VALUES (
     'Jimmy Testuser',
-    'jimmy'
+    'jimmy',
+    '7126237'
   )
   EXCEPT
-  SELECT name, username FROM people;
+  SELECT name, username, uid FROM people;
