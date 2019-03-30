a65daf3a28f2e57b1d87433839506fe975df9f04
nelsonic <nelson+github@dwyl.io>
2019-03-30 23:04:58 +0000

    simplify test

--- test/_create_table.js
+++ test/_create_table.js
@@ -2,27 +2,24 @@ const DBURL = process.env.DATABASE_URL
   || "postgres://postgres:@localhost/codeface";
 const pg = require('pg');
 const client = new pg.Client(DBURL); // Create PostgreSQL Client/Connection
-const test = require('tap').test; // see: github.com/dwyl/learn-tape
+const test = require('tap'); // see: github.com/dwyl/learn-tape
 
-test('confirm that database tables were created', function(t) {
-  client.connect(function(err) {
-    if (err) {
-      console.log(' - - - - - - - - - - - - - - - -> err:')
-      console.error(err);
-      console.log('- - - - - - - - - - - - - - - - - - - ');
-      assert(!err); // "die" if we cannot connect to the database!
-    }
-    else {
-      var SELECT = 'SELECT * FROM people';
-      client.query(SELECT, function (err, result) {
-        if (err) {
-          console.error(err);
-        }
-        console.log('result:', result.rows[0]);
-        t.equal(result.rows[0].username, 'jimmy', 'username is jimmy');
-        client.end(); // close connection to database
-        t.end()
-      });
-    }
-  });
+client.connect(function(err) {
+  if (err) {
+    console.log(' - - - - - - - - - - - - - - - -> err:')
+    console.error(err);
+    console.log('- - - - - - - - - - - - - - - - - - - ');
+    assert(!err); // "die" if we cannot connect to the database!
+  }
+  else {
+    var SELECT = 'SELECT * FROM people';
+    client.query(SELECT, function (err, result) {
+      if (err) {
+        console.error(err);
+      }
+      console.log('result:', result.rows[0]);
+      test.equal(result.rows[0].username, 'jimmy', 'username is jimmy');
+      client.end(); // close connection to database
+    });
+  }
 });
