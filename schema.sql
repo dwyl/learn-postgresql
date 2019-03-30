68ded9013e5658b0417abc9edab2fce5aa7fadfe
nelsonic <nelson+github@dwyl.io>
2019-03-30 15:57:02 +0000

    fix followers CREATE statement with FK! thanks to @markdrago #57

--- schema.sql
+++ schema.sql
@@ -4,29 +4,30 @@ CREATE TABLE IF NOT EXISTS "people" (
 	"name" VARCHAR(50) DEFAULT NULL,
 	"username" VARCHAR(50) NOT NULL UNIQUE,
 	"company" VARCHAR(50) DEFAULT NULL,
-  "uid" INT DEFAULT NULL -- the person's github id e.g: 4185328
+  "uid" INT DEFAULT NULL -- the person's GitHub uid e.g: 4185328
 );
 
 /* insert a person into the people table if it does not already exist */
 /* stackoverflow.com/questions/4069718/postgres-insert-if-does-not-exist */
-INSERT INTO people (name, username)
-  SELECT name, username FROM people
+INSERT INTO people (name, username, company)
+  SELECT name, username, company FROM people
   UNION
   VALUES (
     'Jimmy Testuser',
-    'jimmy'
+    'jimmy',
+    'Great!'
   )
   EXCEPT
-  SELECT name, username FROM people;
+  SELECT name, username, company FROM people;
 
 CREATE TABLE IF NOT EXISTS "followers" (
 	"inserted_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	"follower_id" integer(10) NOT NULL REFERENCES "people"("id")
-    CONSTRAINT "followers_fk0"
-    FOREIGN KEY ("follower_id")
-    REFERENCES "people"("id"),
-	"leader_id" integer(10) NOT NULL REFERENCES "people"("id")
+	"follower_id" INT REFERENCES people (id),
     CONSTRAINT "followers_fk1"
+    FOREIGN KEY ("follower_id")
+    REFERENCES people (id),
+  "leader_id" INT REFERENCES people (id),
+    CONSTRAINT "followers_fk2"
     FOREIGN KEY ("leader_id")
-    REFERENCES "people"("id")
-)
+    REFERENCES people (id)
+);
