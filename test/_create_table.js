54c22dc2cc412d6698e153a12b7c6c076e0e1c9b
nelsonic <nelson+github@dwyl.io>
2019-03-30 22:45:24 +0000

    simplify test/_create by using "psql -a -f schema.sql" command instead https://github.com/dwyl/learn-postgresql/issues/51#issuecomment-478267336

--- test/_create_table.js
+++ test/_create_table.js
@@ -1,37 +1,7 @@
 const DBURL = process.env.DATABASE_URL
   || "postgres://postgres:@localhost/codeface";
-const assert = require('assert');
 const pg = require('pg');
-const resolve = require('path').resolve;
-const read = require('fs').readFileSync;
-const client = new pg.Client(DBURL); // Create PostgreSQL Client
-
-// NEXT: use https://github.com/dwyl/ordem to run the tasks ...
-
-
-const schema = read(resolve('./schema.sql'), 'utf8').toString();
-console.log('\n', schema);
-
-/**
- * create_tables sets up the database tables we need for the MVP app
- * @param  {Function} callback called if/when the SQL script succeeds!
- */
-function create_tables (callback) {
-
-  client.query(schema, function(err, result) {
-    if (err) {
-      console.error(err);
-    }
-    console.info('DB Table Created & Test Data Inserted');
-    var SELECT = 'SELECT * FROM people';
-    client.query(SELECT, function(err, result) {
-
-      client.end(); // close connection to database
-      return callback(err, result);
-    })
-  });
-
-}
+const client = new pg.Client(DBURL); // Create PostgreSQL Client/Connection
 
 client.connect(function(err) {
   if (err) {
@@ -41,11 +11,13 @@ client.connect(function(err) {
     assert(!err); // "die" if we cannot connect to the database!
   }
   else {
-    create_tables(function (err, result) {
+    var SELECT = 'SELECT * FROM people';
+    client.query(SELECT, function (err, result) {
       if (err) {
         console.error(err);
       }
       console.log('result:', result.rows[0]);
+      client.end(); // close connection to database
     });
   }
 });
