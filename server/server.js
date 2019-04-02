90d1a9a8feea42ca6e84ffdcee54ff186a20d762
nelsonic <nelson+github@dwyl.io>
2019-04-03 00:25:27 +0100

    add server.test.js to execute the basic node.js server

--- server/server.js
+++ server/server.js
@@ -6,15 +6,15 @@ http.createServer(function run (req, res) { // can you make simplify it? ;-)
   console.log(req.method, ':', req.url);    // absolute minimum request logging
   var url = req.url.split('?')[0];          // strip query params for routing
   switch (url) {
-    case '/elmo.js':                        // not "DRY" ... #helpwanted!
-      handlers.serve_static(req, res);
-      break;
-    case '/app.js':                         // serve the client application
-      handlers.serve_app(req, res);
-      break;
-    case '/save':                           // save state to server
-      handlers.handle_post(req, res);
-      break;
+    // case '/elmo.js':                        // not "DRY" ... #helpwanted!
+    //   handlers.serve_static(req, res);
+    //   break;
+    // case '/app.js':                         // serve the client application
+    //   handlers.serve_app(req, res);
+    //   break;
+    // case '/save':                           // save state to server
+    //   handlers.handle_post(req, res);
+    //   break;
     default:                                // serve the application
       handlers.serve_index(req, res);
       break;
