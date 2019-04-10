d397650ee88a20346396107ad5a7af563dd30870
nelsonic <nelson+github@dwyl.io>
2019-04-10 23:08:19 +0100

    update version of github-scraper@6.0.0 spider works now. #51

--- server/spider.js
+++ server/spider.js
@@ -4,8 +4,8 @@ const gs = require('github-scraper');
 
 function fetch (path, callback) {
   gs(path, function(err, data) {
-    if (err) { db.exec_cb(callback, err, data) }
-    console.log(data); // or what ever you want to do with the data
+    return db.exec_cb(callback, err, data)
+    // console.log(data); // or what ever you want to do with the data
   })
 }
 
@@ -13,6 +13,6 @@ module.exports = {
   fetch: fetch
 }
 
-gs('torvalds', function(err, data) {
-  console.log(err, data);
-});
+// gs('torvalds', function(err, data) {
+//   console.log(err, data);
+// });
--- test/spider.test.js
+++ test/spider.test.js
@@ -1,9 +1,11 @@
 const test = require('tap').test;
 const spider = require('../server/spider');
+const db = require('../server/db');
 
 test('connect to server', function (t) {
   spider.fetch('dwyl', function(err, data) {
     console.log(err, data);
+    db.end(); // close connection to database
     t.end()
   });
 });
