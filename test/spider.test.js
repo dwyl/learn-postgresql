cff45b874de11255d2b6553f3e04f8a4087a2668
nelsonic <nelson+github@dwyl.io>
2019-04-11 20:35:54 +0100

    update version of github-scraper to 6.2.1 (latest bug fixes) for #51

--- test/spider.test.js
+++ test/spider.test.js
@@ -2,9 +2,9 @@ const test = require('tap').test;
 const spider = require('../server/spider');
 const db = require('../server/db');
 
-test('connect to server', function (t) {
+test('crawl @dwyl org', function (t) {
   spider.fetch('dwyl', function(err, data) {
-    console.log(err, data);
+    console.log(err, data.entries[0]);
     db.end(); // close connection to database
     t.end()
   });
