e2733c3d4554b5ad4cb60c0acda23517458ec993
nelsonic <nelson+github@dwyl.io>
2019-04-14 12:12:36 +0100

    simplify schema.sql (single relationships table) https://github.com/dwyl/learn-postgresql/issues/63

--- schema.sql
+++ schema.sql
@@ -15,53 +15,13 @@ CREATE TABLE IF NOT EXISTS "people" (
 	"recent_activity" INT DEFAULT 0
 );
 
-/* insert a person into the people table if it does not already exist */
-/* stackoverflow.com/questions/4069718/postgres-insert-if-does-not-exist */
-INSERT INTO people (name, username, worksfor, uid)
-  SELECT name, username, worksfor, uid FROM people
-  UNION
-  VALUES (
-    'Jimmy Testuser',
-    'jimmy',
-    'Great!',
-		99
-  )
-  EXCEPT
-  SELECT name, username, worksfor, uid FROM people;
-
-INSERT INTO people (name, username, worksfor, uid)
-  SELECT name, username, worksfor, uid FROM people
-  UNION
-  VALUES (
-    'Ron Swanson',
-    'dukesilver',
-    'OfferManWood',
-		123
-  )
-  EXCEPT
-  SELECT name, username, worksfor, uid FROM people;
-
-CREATE TABLE IF NOT EXISTS "followers" (
+CREATE TABLE IF NOT EXISTS "relationships" (
   "inserted_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "follower_id" INT REFERENCES people (id),
-    CONSTRAINT "followers_fk0"
-    FOREIGN KEY ("follower_id")
-    REFERENCES people (id),
-  "leader_id" INT REFERENCES people (id),
-    CONSTRAINT "followers_fk1"
-    FOREIGN KEY ("leader_id")
-    REFERENCES people (id)
+  "person_id" INT REFERENCES people (id) DEFAULT NULL,
+  "leader_id" INT REFERENCES people (id) DEFAULT NULL,
+  "org_id" INT REFERENCES orgs (id) DEFAULT NULL,
+  "repo_id" INT REFERENCES repos (id) DEFAULT NULL
 );
-
-INSERT INTO followers (follower_id, leader_id)
-  SELECT follower_id, leader_id FROM followers
-  UNION
-  VALUES (
-    2,
-    1
-  )
-  EXCEPT
-  SELECT follower_id, leader_id FROM followers;
 
 CREATE TABLE IF NOT EXISTS "orgs" (
   "inserted_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -76,39 +36,6 @@ CREATE TABLE IF NOT EXISTS "orgs" (
   "uid" INT NOT NULL
 );
 
-INSERT INTO orgs (url, username)
-  SELECT url, username FROM orgs
-  UNION
-  VALUES (
-    'dwyl.com',
-    'dwyl'
-  )
-  EXCEPT
-  SELECT url, username FROM orgs;
-
-
-CREATE TABLE IF NOT EXISTS "members" (
-  "inserted_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "person_id" INT REFERENCES people (id),
-    CONSTRAINT "members_fk0"
-    FOREIGN KEY ("person_id")
-    REFERENCES people (id),
-  "org_id" INT REFERENCES orgs (id),
-    CONSTRAINT "members_fk1"
-    FOREIGN KEY ("org_id")
-    REFERENCES orgs (id)
-);
-
-INSERT INTO members (person_id, org_id)
-  SELECT person_id, org_id FROM members
-  UNION
-  VALUES (
-    1,
-    1
-  )
-  EXCEPT
-  SELECT person_id, org_id FROM members;
-
 CREATE TABLE IF NOT EXISTS "repos" (
 	"inserted_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "id" SERIAL PRIMARY KEY,
@@ -122,26 +49,7 @@ CREATE TABLE IF NOT EXISTS "repos" (
 	"langs" VARCHAR(255) DEFAULT NULL,
 	"tags" TEXT DEFAULT NULL,
 	"person_id" INT REFERENCES people (id), -- can be NULL if repo belongs to org.
-    CONSTRAINT "repos_fk0"
-    FOREIGN KEY ("person_id")
-    REFERENCES people (id),
-	"org_id" INT REFERENCES orgs (id), -- this can be NULL if repo is personal.
-    CONSTRAINT "repos_fk1"
-    FOREIGN KEY ("org_id")
-    REFERENCES orgs (id)
-);
-
-
-CREATE TABLE IF NOT EXISTS "stars" (
-  "inserted_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "person_id" INT REFERENCES people (id), -- only people can "star" repos.
-    CONSTRAINT "stars_fk0"
-    FOREIGN KEY ("person_id")
-    REFERENCES people (id),
-	"repo_id" INT REFERENCES repos (id),
-    CONSTRAINT "stars_fk1"
-    FOREIGN KEY ("repo_id")
-    REFERENCES repos (id)
+	"org_id" INT REFERENCES orgs (id) -- this can be NULL if repo is personal.
 );
 
 CREATE TABLE IF NOT EXISTS "logs" (
